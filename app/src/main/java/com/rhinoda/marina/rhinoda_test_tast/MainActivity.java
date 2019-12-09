333013624e778430bd43bf1565245001a3c008a6
white17werewolf <white.werewolf@mail.ru>
2019-12-09 13:14:52 +0600

    Item 1.10 (EditText)

--- app/src/main/java/com/rhinoda/marina/rhinoda_test_tast/MainActivity.java
+++ app/src/main/java/com/rhinoda/marina/rhinoda_test_tast/MainActivity.java
@@ -3,6 +3,8 @@ package com.rhinoda.marina.rhinoda_test_tast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
     ImageView img;
     Switch sw;
     String  strYasha = "Яша кушал кашу";
-
+    int count = factorial(strYasha.length());
+    //int max =
+    int shift = strYasha.length();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +35,42 @@ public class MainActivity extends AppCompatActivity {
         img = findViewById(R.id.imageView);
         sw = findViewById(R.id.switch1);
 
+//item 1.9
+        btn.setOnClickListener(v -> {strYasha.split(" ");
+        //COMBINATION
+           /* while(count<0){
+                String t = strYasha[shift];
+            }*/
 
-        btn.setOnClickListener(v -> strYasha.split(" "));
+        });
 
+
+//item 1.10
         sw.setOnCheckedChangeListener((buttonView, isChecked) -> { img.setVisibility(isChecked ? View.VISIBLE :  View.GONE); });
+        editTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                btn.setText(editTxt.getText());
+            }
+        });
     }
+
+    public  Integer factorial(int n){
+        return (n>0)?n*factorial(n-1):1;
+    }
+
+
+
+
 }
 
